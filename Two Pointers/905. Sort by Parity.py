f863d57cb536eab70e1f8173be3e55d4d73b658e
ImRuliad <nicholaseovva@csus.edu>
2025-03-13 13:09:16 -0700

    fix comment

--- Two Pointers/905. Sort by Parity.py
+++ Two Pointers/905. Sort by Parity.py
@@ -15,17 +15,7 @@ Output: [0]
 
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        """
-        while left < right
-            if nums[left ptr] % 3 != 0 -> even
-                keep it in place
-                left += 1
-            if nums[left ptr] % 3 == 0 -> odd
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-                right -= 1
-        """
-
+     
         left: int = 0
         right: int = len(nums) - 1
 
