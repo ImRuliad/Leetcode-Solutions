e34c59cf9e4300f6a70fbd833278040124273c35
ImRuliad <nicholaseovva@csus.edu>
2025-07-22 16:45:33 -0700

    solve leetcode 1385

---
+++ Two Pointers/1385. FInd The Distance Value Between Two Arrays.py
@@ -0,0 +1,54 @@
+"""
+Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
+The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
+
+Example 1:
+Input: arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2
+Output: 2
+Explanation: 
+    For arr1[0]=4 we have: 
+    |4-10|=6 > d=2 
+    |4-9|=5 > d=2 
+    |4-1|=3 > d=2 
+    |4-8|=4 > d=2 
+    For arr1[1]=5 we have: 
+    |5-10|=5 > d=2 
+    |5-9|=4 > d=2 
+    |5-1|=4 > d=2 
+    |5-8|=3 > d=2
+    For arr1[2]=8 we have:
+    |8-10|=2 <= d=2
+    |8-9|=1 <= d=2
+    |8-1|=7 > d=2
+    |8-8|=0 <= d=2
+
+Example 2:
+Input: arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3
+Output: 2
+
+Example 3:
+Input: arr1 = [2,1,100,3], arr2 = [-5,-2,10,-3,7], d = 6
+Output: 1
+"""
+
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        arr2.sort()
+        count: int = len(arr1)
+
+        for number in arr1:
+            left: int = 0
+            right: int = len(arr2)
+            
+            while left < right:
+                mid: int = left + (right-left)//2
+                if arr2[mid] < number:
+                    left = mid + 1
+                else:
+                    right = mid
+
+            if left < len(arr2) and abs(number-arr2[left]) <= d:
+                count -= 1
+            elif left > 0 and abs(number-arr2[left-1]) <= d:
+                count -= 1
+        return count
