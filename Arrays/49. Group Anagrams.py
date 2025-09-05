90c3f4472788b00ee1e558081f241b7759519a6f
ImRuliad <nicholaseovva@csus.edu>
2025-09-05 16:20:50 -0700

    solve group anagrams

---
+++ Arrays/49. Group Anagrams.py
@@ -0,0 +1,35 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+"""
+
+from collections import Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+
+        for word in strs:
+            freq_word = tuple(sorted(Counter(word).items()))
+
+            if freq_word in groups:
+                groups[freq_word].append(word)
+            else:
+                groups[freq_word] = [word]
+
+        return list(groups.values())
